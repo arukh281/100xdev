af3bc3478741b8bdf2e7f07ef2574c6f5c89561e
arukh281 <arukhandelwal281@gmail.com>
2024-02-15 19:24:08 +0530

    15/02/24

---
+++ week3/3.3/bank_example.js
@@ -0,0 +1,12 @@
+// 1. Kirat goes open a bank account
+// 2. Kirat deposits some money in
+// 3. kirat gets back a chequr book from the bank
+
+// *********
+
+// 1. whenver i have to pay someone, i sign the cheque
+// 2. everyone can see me check
+// 3. people can try to do the same on their cheque book
+// 4. bank wont accept theirs, will only accept my cheque
+
+// MY CHECK BOOK = MY JSON WEB TOKEN
